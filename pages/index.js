7165b47b22c76dd51286e071676a111ee3a1f023
Shelley Chen <shelleychen318@gmail.com>
2023-02-06 12:23:50 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-semibold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Movie Details App
+      </h2>
+      <h3 className="text-secondary text-2xl font-semibold tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Get info about any movie.
+      </h3>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  // darkMode: 'class', // or 'media' or 'class'
   theme: {
+    screens: {
+      md: { max: "767px" },
+    },
+    // fontFamily: {
+    //   raleway: ["Poppins", "sans-serif"],
+    // },
+    colors: {
+      bc: "#00005C",
+      primary: "#fAF9F6",
+      secondary: "#a16e83",
+      danger: "#1A67F8",
+      lightGrey: "#C9D5EE",
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
