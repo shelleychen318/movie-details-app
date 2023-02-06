0f30e6fa4a66ef4e0c7612907a1660cae88db890
Shelley Chen <shelleychen318@gmail.com>
2023-02-06 14:13:22 -0500

    display movie info

--- pages/api/movie.js
+++ pages/api/movie.js
@@ -1,20 +1,70 @@
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://online-movie-database.p.rapidapi.com/title/find",
-  params: { q: "game of thr" },
-  headers: {
-    "X-RapidAPI-Key": "fb79d0b61bmsh8c210b58b25582ep1c2034jsndd9dae2bfb32",
-    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
-  },
-};
+// const options = {
+//   method: "GET",
+//   url: "https://online-movie-database.p.rapidapi.com/title/find",
+//   params: { q: "game of thr" },
+//   headers: {
+//     "X-RapidAPI-Key": "fb79d0b61bmsh8c210b58b25582ep1c2034jsndd9dae2bfb32",
+//     "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+//   },
+// };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+// export default async function handler(req, res) {
+//   if (req.method === "GET") {
+//     const options = {
+//       method: "GET",
+//       url: "https://online-movie-database.p.rapidapi.com/title/find",
+//       params: { q: req.query.movieName },
+//       headers: {
+//         "X-RapidAPI-Key": "online-movie-database.p.rapidapi.com",
+//         "X-RapidAPI-Host": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+//       },
+//     };
+//     axios
+//       .request(options)
+//       .then(function (response) {
+//         res.status(200).json(response.data);
+//       })
+//       .catch(function (error) {
+//         console.error(error);
+//         res.status(response.status);
+//       });
+//   } else {
+//     res.status(400);
+//   }
+// }
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const options = {
+      method: "GET",
+      url: "https://online-movie-database.p.rapidapi.com/title/find",
+      params: { q: req.query.movieName },
+      headers: {
+        "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+      },
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+        res.status(response.status);
+      });
+  } else {
+    res.status(400);
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 import axios from "axios";
 
 export default function Home() {
   const [movieName, setMovieName] = useState(null); // state to store user input
   const [movieInfo, setMovieInfo] = useState(null); // state to store movie info received from API
+
+  // fetch movie info from IMDB API
+  const fetchMovieInfo = async () => {
+    try {
+      const res = await axios.get(`http://localhost:3000/api/movie`, {
+        params: { movieName },
+      });
+      const { data } = res;
+      const { results } = data;
+      setMovieInfo(results[0]);
+      console.log(movieInfo);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center relative min-h-screen">
@@ -19,12 +36,44 @@ export default function Home() {
             type="text"
             className="border-none outline-none w-2/5 bg-primary px-4 py-2 rounded-lg md:w-full"
             placeholder="Search for a movie..."
-            onChange={(e) => setMovieName(e.target.value)} // event handler to update state value as user types 
+            onChange={(e) => setMovieName(e.target.value)} // event handler to update state value as user types
           />
-          <button className="font-medium ml-4 px-12 py-2 rounded-lg bg-button text-primary transition duration-200 hover:bg-button-dark md:ml-0 md:mt-4">
+          <button
+            className="font-medium ml-4 px-12 py-2 rounded-lg bg-button text-primary transition duration-200 hover:bg-button-dark md:ml-0 md:mt-4"
+            onClick={fetchMovieInfo}
+          >
             Search
           </button>
         </div>
+        {movieInfo && (
+          <div className="flex mt-12 w-3/6 h-4/5 border border-primary md:flex-col md:w-4/6 md:h-full md:mb-12">
+            <Image
+              src={movieInfo.image.url}
+              width={220}
+              height={300}
+              alt={movieInfo.title}
+            />
+            <div className="flex flex-col justify-center ml-8 md:mt-6">
+              <h2 className="text-primary text-xl text-raleway font-bold tracking-wider md:text-base">
+                <span className="text-secondary uppercase">Title: </span>{" "}
+                {movieInfo.title}
+              </h2>
+              <h2 className="text-primary text-xl text-raleway font-bold mt-8 tracking-wider md:text-base md:mt-6">
+                <span className="text-secondary uppercase">Year: </span>{" "}
+                {movieInfo.year}
+              </h2>
+              <h2 className="text-primary text-xl text-raleway font-bold mt-8 tracking-wider md:text-base md:mt-6">
+                <span className="text-secondary uppercase">Run Time: </span>{" "}
+                {movieInfo.runningTimeInMinutes}
+              </h2>
+              <Link href={`https://www.imdb.com${movieInfo.id}`}>
+                <button className="outline-none border border-danger font-bold font-raleway mt-8 px-12 py-2 rounded-sm bg-danger text-primary transition duration-300 hover:bg-bc hover:text-black md:ml-0 md:my-4">
+                  Visit on IMDB
+                </button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
